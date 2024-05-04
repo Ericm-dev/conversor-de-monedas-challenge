614d77421ade89f2acc37747d716ae7960819699
Eric M <ericmerel07@gmail.com>
2024-05-04 12:43:43 -0500

    Actializacion

--- src/com/challenge/conversor/monedas/ConversorDeMonedasAplicacion.java
+++ src/com/challenge/conversor/monedas/ConversorDeMonedasAplicacion.java
@@ -2,7 +2,7 @@ package com.challenge.conversor.monedas;
 
 import com.challenge.conversor.monedas.principal.Principal;
 
-public class ConversorDeDivisasApplication {
+public class ConversorDeMonedasAplicacion {
     public static void main(String[] args) {
         Principal principal = new Principal();
         principal.muestraElMenu();
